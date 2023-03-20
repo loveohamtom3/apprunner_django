694b7a88806408452fb5f8b2b9877c5619bef84c
loveohamtom3 <lovephamtom@icloud.com>
2023-03-20 12:08:52 +0900

    p

--- portfolio/settings/common.py
+++ portfolio/settings/common.py
@@ -82,7 +82,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "myapp/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
