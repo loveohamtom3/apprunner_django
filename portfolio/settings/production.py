8935dd02acefb905dd0daa4e6dfcff6320bd125f
loveohamtom3 <lovephamtom@icloud.com>
2023-03-24 12:54:57 +0900

    p

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -23,6 +23,6 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS           = ['169.254.175.250']
-ALLOWED_HOSTS           = ['xakezxsmjn.ap-northeast-1.awsapprunner.com']
-CSRF_TRUSTED_ORIGINS = ['https://xakezxsmjn.ap-northeast-1.awsapprunner.com']
+ALLOWED_HOSTS           = [ "xakezxsmjn.ap-northeast-1.awsapprunner.com" ]
+ALLOWED_HOSTS           = [ "169.254.175.250" ]
+CSRF_TRUSTED_ORIGINS    = [ "https://xakezxsmjn.ap-northeast-1.awsapprunner.com" ]
