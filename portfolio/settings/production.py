a1aeed2057da60aae35e94bb47afe6c009fa5f40
loveohamtom3 <lovephamtom@icloud.com>
2023-03-24 12:45:40 +0900

    p

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -23,5 +23,6 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS           = [ "169.254.175.250" ]
+ALLOWED_HOSTS           = ['169.254.175.250']
+ALLOWED_HOSTS           = ['xakezxsmjn.ap-northeast-1.awsapprunner.com']
 CSRF_TRUSTED_ORIGINS = ['https://xakezxsmjn.ap-northeast-1.awsapprunner.com']
