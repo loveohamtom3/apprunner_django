f063f3f57f17220301dc90bc7b2ff1bbbb70ea86
loveohamtom3 <lovephamtom@icloud.com>
2023-03-24 13:06:19 +0900

    a

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -23,6 +23,6 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS           = [ "xakezxsmjn.ap-northeast-1.awsapprunner.com" ]
-ALLOWED_HOSTS           = [ "169.254.175.250" ]
 CSRF_TRUSTED_ORIGINS    = [ "https://xakezxsmjn.ap-northeast-1.awsapprunner.com" ]
